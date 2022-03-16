d3f4b36f797c480df4a9f5db7b111cb697e79600
JdeDev <julien@jdedev.fr>
2022-03-16 18:10:52 +0100

    config sqlserver ok

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const sql = require('mssql')
 
 app.use(express.static("./static"))
+
+const sqlConfig = {
+    user: 'sa',
+    password: 'sa',
+    database: "SIGB",
+    server: 'localhost\\FELIX',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+}
 
 //Gestion des templates dans le repertoire vue et utilisation d'ejs
 app.set('views', './template');
@@ -14,6 +31,22 @@ app.get('/', (req, res) => {
 
 app.get('/testAPI', (req, res) => {
     res.status(200).json({ user: "julien", retour: "page OK" })
+})
+
+app.get('/testbdd', (req, res) => {
+    res.send("ok")
+    // make sure that any items are correctly URL encoded in the connection string
+    sql.connect(sqlConfig).then(() => {
+        console.log("connexion ok");
+        sql.query(`select * from Auteur`).then((result) => {
+            console.log(result)
+        })
+            .catch((err) => console.log(err))
+    }).catch((err) => {
+        // ... error checks
+        console.error(err)
+    })
+
 })
 
 app.listen(process.env.PORT | port, () => {
